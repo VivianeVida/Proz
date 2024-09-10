6c07572a9c23084f2b7e296cbac21cd79b9cb4a5
Viviane Vida <67081266+VivianeVida@users.noreply.github.com>
2024-09-10 18:32:54 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-//Este código utiliza o método forEach para iterar sobre cada elemento do array numerosDaSorte. Em cada iteração, ele verifica se o número é par e menor que 50, apenas menor que 50, ou maior que 50, e imprime a frase correspondente.
-
-//Você pode copiar e colar este código no OneCompiler para testá-lo. Se precisar de mais alguma coisa, estou aqui para ajudar!
+// Este código utiliza o método forEach para iterar sobre cada elemento do array numerosDaSorte. Em cada iteração, ele verifica se o número é par e menor que 50, apenas menor que 50, ou maior que 50, e imprime a frase correspondente.
+// Você pode copiar e colar este código no OneCompiler para testá-lo. Se precisar de mais alguma coisa, estou aqui para ajudar!
 
 
 let numerosDaSorte = [37, 14, 26, 5, 94, 87];
